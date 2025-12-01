05477d8ee00c220d7d8213b2b32e59e3a65794a9
salma alsinan <salma.mu.s@gmail.com>
2025-12-01 14:12:53 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#training loop
+python3 train.py --model=unet --problem=firstbreak --noise_type=0 --noise_scale=0.25 --device=0 --epochs=100 --lr=5e-5 --batch_size=16
+
+
+#evaluation to generate the robustness matrix
+python3 evaluate_full_matrix.py --model_type=unet --problem=firstbreak --noise_type1=0 --noise_scale1=0.25 --metadata=../metadata/ --savepath=../metadata/evaluation/
+
+
+#conver tensor board logs to csv 
+
+python3 logs_to_csv.py --problem=firstbreak 
